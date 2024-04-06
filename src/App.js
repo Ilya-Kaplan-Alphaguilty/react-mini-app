051fe3ddbfa0af40682c5571141cb2edaa148d70
Ilya Kaplan <ik@alphaguilty.io>
2024-04-06 18:53:33 +0100

    test

--- src/App.js
+++ src/App.js
@@ -24,6 +24,9 @@ function App() {
 
   return (
     <div className="App" ref={setRef}>
+      <button onClick={() => window.open("www.google.com", "_blank")}>
+        test
+      </button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
